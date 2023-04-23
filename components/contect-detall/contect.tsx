2c457e49af2db40880a92ebe2d3150d3c4e29652
pampamz <129577683+pampamz@users.noreply.github.com>
2023-04-23 09:45:13 +0000

    icon&font

--- components/contect-detall/contect.tsx
+++ components/contect-detall/contect.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { MdAttachEmail } from "react-icons/md";
+import { IoMdCall } from "react-icons/io";
+import { AiOutlineMessage,AiFillHome } from "react-icons/ai";
 
 interface ContectDetailProps {}
 
@@ -7,7 +9,7 @@ const ContectDetail: React.FC<ContectDetailProps> = ({}) => {
   function detil(icon: React.ReactNode, name: string) {
     return (
       <>
-        <div className=" flex p-3 gap-7 bg-slate-400">
+        <div className=" flex p-3 gap-7 ">
           <div className="flex items-center justify-center text-gray-700 ">
             {icon}
             {/* <MdAttachEmail className="mt-1  text-2xl "></MdAttachEmail> */}
@@ -27,9 +29,16 @@ const ContectDetail: React.FC<ContectDetailProps> = ({}) => {
         <MdAttachEmail className="mt-1  text-2xl "></MdAttachEmail>,
         "pammy.kanokwan@gmail.com"
       )}
+      {detil(<IoMdCall className="mt-1  text-2xl "></IoMdCall>, "065-8186733")}
+
       {detil(
-        <MdAttachEmail className="mt-1  text-2xl "></MdAttachEmail>,
-        "pammy.kanokwan@gmail.com"
+        <AiOutlineMessage className="mt-1  text-2xl "></AiOutlineMessage>,
+        " ID:kanokwan.pam"
+      )}
+
+      {detil(
+        <AiFillHome className="mt-1  text-2xl "></AiFillHome>,
+        "หมู่ที่ 9 ต.เทอดไทย อ.ทุ่งเขาหลวง จ.ร้อยเอ็ด"
       )}
     </>
   );
