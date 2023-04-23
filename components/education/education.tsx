2e4a09763dd13f92fca8464354389c92d0cb9122
pampamz <129577683+pampamz@users.noreply.github.com>
2023-04-23 12:20:29 +0000

    gis1

--- components/education/education.tsx
+++ components/education/education.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-interface EducationProps {
-
-}
+type EducationProps = {
+};
 
 const Education: React.FC<EducationProps> = ({  }) => {
-    return (
-        <> 
-      <div className="flex flex-col gap-5 p-5">
+  return (
+    <>
+       <div className="flex flex-col gap-5 p-5">
         <div className=" bg-slate-100 p-1.5 w-fit text-3xl font-bold text-slate-950">
         Education </div>
         <div className="flex flex-col gap-3 p-2">
           <div className=" text-2xl font-bold text-slate-950">Mahasarakham University [2018-2021]</div>
           <div className=" text-1xl font-bold text-slate-950">Geographic information system</div>
-          <div className=" text-xl font-bold text-slate-950"></div>i
+          <div className=" text-xl font-bold text-slate-950"></div>
+          </div>
+          </div>
+    </>
+  );
+};
+
+export default Education;
