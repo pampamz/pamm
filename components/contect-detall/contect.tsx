cedb0b4792ac9053220210b0ed30c20218b02c8e
pampamz <129577683+pampamz@users.noreply.github.com>
2023-04-23 09:19:20 +0000

    function

--- components/contect-detall/contect.tsx
+++ components/contect-detall/contect.tsx
@@ -1,16 +1,36 @@
 import React from "react";
+import { MdAttachEmail } from "react-icons/md";
 
 interface ContectDetailProps {}
 
 const ContectDetail: React.FC<ContectDetailProps> = ({}) => {
+  function detil(icon: React.ReactNode, name: string) {
+    return (
+      <>
+        <div className=" flex p-3 gap-7 bg-slate-400">
+          <div className="flex items-center justify-center text-gray-700 ">
+            {icon}
+            {/* <MdAttachEmail className="mt-1  text-2xl "></MdAttachEmail> */}
+          </div>
+          <div className=" text-center text-lg text-gray-800 font-mono ">
+            {name}
+            {/* pammy.kanokwan@gmail.com */}
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      <div className=" flex p-3 gap-3 bg-slate-400">
-        <div className="text-lg text-gray-700 ">o</div>
-        <div className=" text-center text-lg text-gray-800 font-mono ">
-          pammy.kanokwan@gmail.com
-        </div>
-      </div>
+      {detil(
+        <MdAttachEmail className="mt-1  text-2xl "></MdAttachEmail>,
+        "pammy.kanokwan@gmail.com"
+      )}
+      {detil(
+        <MdAttachEmail className="mt-1  text-2xl "></MdAttachEmail>,
+        "pammy.kanokwan@gmail.com"
+      )}
     </>
   );
 };
