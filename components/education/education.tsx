695ad4f28e4adf06f308deff0001c14f0bddfcf6
pampamz <129577683+pampamz@users.noreply.github.com>
2023-04-23 12:14:05 +0000

    ytg
    TTG#   (use "git push" to publish your local commits)

--- components/education/education.tsx
+++ components/education/education.tsx
@@ -11,13 +11,6 @@ const Education: React.FC<EducationProps> = ({  }) => {
         <div className=" bg-slate-100 p-1.5 w-fit text-3xl font-bold text-slate-950">
         Education </div>
         <div className="flex flex-col gap-3 p-2">
-          <div className=" text-xl font-bold text-slate-950">Mahasarakham University [2018-2021]</div>
-          <div className=" text-xl font-bold text-slate-950">Geographic information system</div>
-  
-      </div>
-      </div>
-        </>
-    );
-};
-
-export default Education;
+          <div className=" text-2xl font-bold text-slate-950">Mahasarakham University [2018-2021]</div>
+          <div className=" text-1xl font-bold text-slate-950">Geographic information system</div>
+          <div className=" text-xl font-bold text-slate-950"></div>i
