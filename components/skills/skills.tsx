505dc4a87df12d4f15c3abeff7f7be24b8c656f6
pampamz <129577683+pampamz@users.noreply.github.com>
2023-04-23 10:41:39 +0000

    skill1

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -5,13 +5,15 @@ interface SkillsProps {}
 const Skills: React.FC<SkillsProps> = ({}) => {
   return (
     <>
-    <div className="gap-9 p-5">
+    <div className="flex flex-col gap-5 p-5">
       <div className=" bg-slate-100 p-1.5 w-fit text-3xl font-bold text-slate-950">
         Skills </div>
-        <div className="gap-10 p-5">
-        <div className=" text-2xl font-bold text-slate-950">HTML </div>
-        <div className=" text-2xl font-bold text-slate-950">CSS</div>
+        <div className="flex flex-col gap-3 p-2">
+        <div className=" text-xl font-bold text-slate-950">HTML </div>
+        <div className=" text-xl font-bold text-slate-950">TaiwindCSS</div>
+        <div className="text-xl font-bold text-slate-950">NextJs</div>
         </div>
+        
 
     
         </div>
